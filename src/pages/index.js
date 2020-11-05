a6a66dae4161c5338a0a9ee2171d5b63e3220cf5
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-11-05 12:53:44 -0500

    updated meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
+import img from '../images/metaImg.png';
 
 export default () => {
   const { title, lang, description } = headData;
@@ -11,10 +12,29 @@ export default () => {
   return (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
+        {/* <!-- HTML Meta Tags --> */}
         <title>{title}</title>
-        <html lang={lang || 'en'} />
         <meta name="description" content={description} />
+        <meta charSet="utf-8" />
+        <html lang={lang || 'en'} />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={img} />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="http://www.whoisclayton.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={img} />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={img} />
       </Helmet>
       <App />
     </>
