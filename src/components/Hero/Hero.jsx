8fbc576a810a6b16332929257a814dccc8f4c6c0
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-12-01 09:53:16 -0800

    remove particleBackground from mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -24,7 +24,7 @@ const Header = () => {
 
   return (
     <section id="hero" className="jumbotron">
-      <ParticleBackGround />
+      {isDesktop ? <ParticleBackGround /> : null}
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
